b0face193d0584a95bdf0d11a75d7ea0e22d40ba
Mehadi Hasan <Mehadihasan.phy@gmail.com>
2022-05-11 13:58:15 +0600

    Navbar Added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './Pages/Shared/Navbar';
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 import Login from './Pages/Login/Login';
+import Contact from './Pages/Contact/Contact';
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="appontment" element={<About />} />
         <Route path="reviews" element={<About />} />
-        <Route path="contact" element={<About />} />
+        <Route path="contact" element={<Contact></Contact>} />
         <Route path="about" element={<About />} />
         <Route path="login" element={<Login />} />
       </Routes>
